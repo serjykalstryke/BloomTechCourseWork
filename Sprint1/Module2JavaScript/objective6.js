756c1d988a3371b5c0f02b1e8b1d6ead41e02cd7
David Stinnett <davidstinnett@icloud.com>
2023-05-21 22:10:17 -0400

    started module 3 on arrays by completing objective 1

---
+++ Sprint1/Module2JavaScript/objective6.js
@@ -0,0 +1,14 @@
+//Objective 6 - Write and Call Functions using Arguments, Parameters, and Return Statement
+
+//Overview: 
+//Passing Data into Functions
+
+//Let's return to our cookie example. Pretend that you want your robot to bake different types of cookies. Do you need to write a new function for each type of cookie? No! You can pass in arguments to your function to change the behavior of the function. Consider the following:
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+add(2,2)
+add(4,5)
+
